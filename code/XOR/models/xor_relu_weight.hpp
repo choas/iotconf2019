d26f6f8d1aa4b647b6ff14b194e31bd39d129a78
Lars Gregori <lars.gregori@sap.com>
2019-09-17 22:53:01 +0200

    adjust notebooks and new run

--- code/XOR/models/xor_relu_weight.hpp
+++ code/XOR/models/xor_relu_weight.hpp
@@ -8,7 +8,7 @@ const int inline_MatMul_eightbit_Placeholder__port__0_reshape_dims_0 [ 1 ] = {  
 const int inline_MatMul_eightbit_Placeholder__port__0_reduction_dims_0 [ 1 ] = {  0,  };
 #include <stdint.h>
 
-const float inline_Variable_0 [ 20 ] = {  -0.86062706,  -0.08979624,  2.4867814,  -1.3065245,  0.6820757,  0.2221431,  0.17818943,  0.23360021,  -1.442773,  -2.0707037,  0.860596,  0.010079716,  -2.486868,  -1.306502,  0.8593252,  0.30453673,  0.9356374,  0.20003404,  1.4427375,  2.0706797,  };
+const float inline_Variable_0 [ 20 ] = {  -2.235275,  -1.0766231,  0.061877325,  -0.008710417,  -0.15125018,  -0.8288693,  0.14909211,  -1.1520185,  0.040085774,  2.771753,  2.2353868,  1.0766797,  0.43723008,  0.026936838,  0.031320963,  0.8288856,  -0.14909166,  1.1520101,  -0.07423007,  -2.7716079,  };
 #include <stdint.h>
 
 const int inline_MatMul_eightbit_Variable__port__0_reshape_dims_0 [ 1 ] = {  -1,  };
@@ -17,7 +17,7 @@ const int inline_MatMul_eightbit_Variable__port__0_reshape_dims_0 [ 1 ] = {  -1,
 const int inline_MatMul_eightbit_Variable__port__0_reduction_dims_0 [ 1 ] = {  0,  };
 #include <stdint.h>
 
-const float inline_Variable_1_0 [ 10 ] = {  -2.8105984e-05,  -0.011069158,  -8.873433e-05,  1.3064694,  -0.6820682,  0.42137596,  0.8437028,  0.211507,  -4.2146767e-05,  -3.884506e-05,  };
+const float inline_Variable_1_0 [ 10 ] = {  -5.946523e-05,  -5.799502e-06,  1.5188397,  0.5028262,  -0.037024926,  -2.7838428e-06,  -1.1657356e-06,  5.936658e-06,  -0.040643256,  -5.085444e-05,  };
 #include <stdint.h>
 
 const int inline_add_eightbit_Variable_1__port__0_reshape_dims_0 [ 1 ] = {  -1,  };
@@ -26,7 +26,7 @@ const int inline_add_eightbit_Variable_1__port__0_reshape_dims_0 [ 1 ] = {  -1, 
 const int inline_add_eightbit_Variable_1__port__0_reduction_dims_0 [ 1 ] = {  0,  };
 #include <stdint.h>
 
-const float inline_Variable_2_0 [ 20 ] = {  -0.86714303,  0.8312849,  0.057392508,  -0.025885789,  -2.462204,  2.4905005,  1.6327465,  -1.5586045,  0.8769306,  -0.93042624,  0.36877942,  -0.38657248,  0.9086818,  -0.8887023,  0.18673089,  -0.2828411,  -1.5274271,  1.341296,  -2.0902328,  2.0159824,  };
+const float inline_Variable_2_0 [ 20 ] = {  -2.1585546,  2.280012,  -1.1020154,  1.0231366,  1.2166363,  -1.0185888,  0.3109968,  -0.38579237,  0.116573356,  0.033069503,  -0.83664715,  0.80556124,  -0.06830558,  0.21974804,  -1.0603178,  1.2306156,  0.1706827,  -0.0451063,  -2.719408,  2.7992096,  };
 #include <stdint.h>
 
 const int inline_MatMul_1_eightbit_Variable_2__port__0_reshape_dims_0 [ 1 ] = {  -1,  };
@@ -35,7 +35,7 @@ const int inline_MatMul_1_eightbit_Variable_2__port__0_reshape_dims_0 [ 1 ] = { 
 const int inline_MatMul_1_eightbit_Variable_2__port__0_reduction_dims_0 [ 1 ] = {  0,  };
 #include <stdint.h>
 
-const float inline_Variable_3_0 [ 2 ] = {  0.8522662,  -0.8466811,  };
+const float inline_Variable_3_0 [ 2 ] = {  1.6034027,  -1.800267,  };
 #include <stdint.h>
 
 const int inline_add_1_eightbit_Variable_3__port__0_reshape_dims_0 [ 1 ] = {  -1,  };
